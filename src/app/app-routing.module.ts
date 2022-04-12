2339d0354ea4c1407b1f44624461cc9d764a0ccb
BenjaminGANDRILLE <b.gandrille83@gmail.com>
2022-04-12 08:55:52 -0300

    path maj pour mobile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,25 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'tabs',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: '',
+    loadChildren: () => import('./tab1/tab1.module').then(m => m.Tab1PageModule)
+  },
+  {
+    path: 'tabs/tab1',
+    loadChildren: () => import('./tab1/tab1.module').then(m => m.Tab1PageModule)
+  },
+  {
+    path: 'tabs/tab2',
+    loadChildren: () => import('./tab2/tab2.module').then(m => m.Tab2PageModule)
+  },
+
+  {
+    path: 'tabs/tab3',
+    loadChildren: () => import('./tab3/tab3.module').then(m => m.Tab3PageModule)
   },
   {
     path: 'detail-revue',
